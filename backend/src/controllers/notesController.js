0699205c21f4dff4035b1db555bae2dc07ad6141
suprajanj <jsuprajan@gmail.com>
2025-07-28 19:35:03 +0530

    finish

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import Note from '../models/Note.js';
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
     try{
-        const notes = await Note.find().sort({createdAt: -1});
+        const notes = await Note.find().sort({createdAt: -1}); // -1 will sort in dec order newest first
         res.status(200).json(notes);
     }
     catch(error){
